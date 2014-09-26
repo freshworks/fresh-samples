a1ffccffb7bc5bf590310536a1a1a7c877467c81
Subhash Chandran <subwiz@gmail.com>
2014-09-26 08:43:40 +0530

    example for create ticket with attachment

---
+++ python_samples/create_ticket_attachment.py
@@ -0,0 +1,20 @@
+## This script requires "requests": http://docs.python-requests.org/
+## To install: pip install requests
+
+import requests
+
+FRESHDESK_ENDPOINT = "http://YOUR_DOMAIN.freshdesk.com" # check if you have configured https, modify accordingly
+FRESHDESK_KEY = "YOUR_API_KEY"
+
+multipart_data = {
+    'helpdesk_ticket[email]': ('', 'example@example.com'),
+    'helpdesk_ticket[subject]': ('', 'Ticket Title'),
+    'helpdesk_ticket[description]': ('', 'Ticket description.'),
+    'helpdesk_ticket[attachments][][resource]': open('create_ticket_attachment.py', 'rb')
+}
+r = requests.post(FRESHDESK_ENDPOINT + '/helpdesk/tickets.json',
+        auth=(FRESHDESK_KEY, "X"),
+        files=multipart_data)
+
+print r.status_code
+print r.content
