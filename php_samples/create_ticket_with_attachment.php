924bb02efb6236bf6dfcbb0499cce7afa3d93997
Subhash Chandran <subwiz@gmail.com>
2014-10-09 23:20:40 +0530

    modified to use CURLFile object as suggested in PHP 5.5 and above

--- php_samples/create_ticket_with_attachment.php
+++ php_samples/create_ticket_with_attachment.php
@@ -1,34 +1,39 @@
 <?php 
 
+$API_KEY = "YOUR_API_KEY";
+$FD_ENDPOINT = "http://YOUR_DOMAIN.freshdesk.com"; // verify if you are using https, and change accordingly!
+
 $email="sample@freshdesk.com";
 $password="test";
 
-$jsondata = array (
-	'helpdesk_ticket[email]' => 'test@freshdesk.com',
-	'helpdesk_ticket[subject]' => 'test',
-	'helpdesk_ticket[description]' => 'testing description content',
-	'helpdesk_ticket[attachments][][resource]' =>  "@" . "/Users/johnpaul/Examples/tommy1.jpg"
+$payload = array(
+  'helpdesk_ticket[email]' => 'test@example.com',
+  'helpdesk_ticket[subject]' => 'test',
+  'helpdesk_ticket[description]' => 'testing description content',
+  // php5.4 & below: 'helpdesk_ticket[attachments][][resource]' =>  "@data/x.png"
+  'helpdesk_ticket[attachments][][resource]' =>  curl_file_create("data/x.png", "image/png", "x.png")
 );
 $header[] = "Content-type: multipart/form-data";
 
-$url = 'http://yourcompany.freshdesk.com/helpdesk/tickets.json';
+$url = "$FD_ENDPOINT/helpdesk/tickets.json";
+
 $ch = curl_init ($url);
 
 curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS,$jsondata);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
 
-curl_setopt($ch, CURLOPT_USERPWD, "$email:$password");
+curl_setopt($ch, CURLOPT_USERPWD, "$API_KEY:X");
 curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-curl_setopt($connection, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_HEADER, false);
 
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-$server_output = curl_exec ($ch);
+$server_output = curl_exec($ch);
 
 $response = json_decode($server_output);
-echo "RESPONSE:<br/>".var_dump($response);
+var_dump($response);
 
 curl_close ($ch);
 
