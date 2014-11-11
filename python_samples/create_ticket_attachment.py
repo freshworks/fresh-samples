35cfd5d9d79d54982654f7e430d9200267f3e836
Subhash Chandran <subwiz@gmail.com>
2014-11-11 15:13:15 +0530

    python example updated with Content-Type for each part, and custom filename.

--- python_samples/create_ticket_attachment.py
+++ python_samples/create_ticket_attachment.py
@@ -6,13 +6,13 @@ import requests
 FRESHDESK_ENDPOINT = "http://YOUR_DOMAIN.freshdesk.com" # check if you have configured https, modify accordingly
 FRESHDESK_KEY = "YOUR_API_KEY"
 
-multipart_data = (
+multipart_data = [
     ('helpdesk_ticket[email]', ('','example@example.com')),
     ('helpdesk_ticket[subject]', ('', 'Ticket Title')),
     ('helpdesk_ticket[description]', ('', 'Ticket description.')),
-    ('helpdesk_ticket[attachments][][resource]', open('create_ticket_attachment.py', 'rb')),
-    ('helpdesk_ticket[attachments][][resource]', open('/etc/hosts', 'rb')),
-)
+    ('helpdesk_ticket[attachments][][resource]', ('create_ticket_attachment.py', open('create_ticket_attachment.py', 'rb'), 'text/plain')),
+    ('helpdesk_ticket[attachments][][resource]', ('logo.png', open('logo.png', 'rb'), 'image/png'))
+]
 r = requests.post(FRESHDESK_ENDPOINT + '/helpdesk/tickets.json',
         auth=(FRESHDESK_KEY, "X"),
         files=multipart_data)
