bfb122add44e630f39bbbfd4ba318ad2d66b68ac
RashmiRam <rashmi@freshdesk.com>
2014-11-20 10:56:47 +0530

    Added update_user_with_avatar file as example for including multipart data in PUT request.
    Signed-off-by: RashmiRam <rashmi@freshdesk.com>

---
+++ Ruby_api_samples/update_user_with_avatar.rb
@@ -0,0 +1,13 @@
+#Update avatar of contact.
+require "rubygems"
+require "rest_client"
+require "json"
+
+# eg:
+#site = RestClient::Resource.new("http://mycompany.freshdesk.com/contacts/5123.json","sample@freshdesk.com","test")
+site = RestClient::Resource.new("http://domain.freshdesk.com/contacts/[user_id].json","sample@freshdesk.com","test")
+
+# response = site.put({:user=>{:name=>"newname",:customer_id=>8}},:content_type=>"application/json")
+
+response = site.put({:user=>{:avatar_attributes=>{:content=>File.open("Mypic.png")}}},:content_type=>"application/json")
+puts "response: #{response.code} \n #{response.body}"
