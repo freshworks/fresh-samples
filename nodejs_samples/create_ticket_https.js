c4468c9aa94135a78318a821d7d1edc5349607ea
RashmiRam <rashmi@freshdesk.com>
2014-11-27 14:24:44 +0530

    Added node.js sample for ssl enabled domains.
    Signed-off-by: RashmiRam <rashmi@freshdesk.com>

---
+++ nodejs_samples/create_ticket_https.js
@@ -0,0 +1,29 @@
+var https = require("https");
+
+var API_KEY = "YOUR-API-KEY";
+var FD_ENDPOINT = "domain.freshdesk.com";
+var ticketBody = {
+  "helpdesk_ticket":{
+    "subject":"Ticket title",
+    "description":"Ticket description.",
+    "email":"nice@gh.com",
+    "priority":1,
+    "status":2
+  }
+}
+
+var params = {
+  hostname : FD_ENDPOINT ,
+  path : "/helpdesk/tickets.json",
+  method: "POST",
+  headers: {"Content-type": "application/json",
+            "Content-length":JSON.stringify(ticketBody).length},
+  auth : API_KEY + ":X"  
+        
+}
+
+var req = https.request(params);
+req.write(JSON.stringify(ticketBody));
+var res = req.end();
+
+console.log(req,res);
