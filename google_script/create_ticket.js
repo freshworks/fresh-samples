6b4afcecd7851fa591219c5c3100b6f652baf784
Subhash Chandran <subwiz@gmail.com>
2014-12-03 11:18:51 +0530

    updated example with base64 encoding

--- google_script/create_ticket.js
+++ google_script/create_ticket.js
@@ -1,25 +1,26 @@
-function testCreateCategory() {
-
+function testCreateTicket() {
+  
   var API_KEY = 'YOUR_API_KEY';
   var ENDPOINT = 'http://YOUR_DOMAIN.freshdesk.com'; // change to https if SSL is configured.
-
+  
   var headers = {
     'Content-type': 'application/json',
     'Authorization': 'Basic ' + Utilities.base64Encode(API_KEY + ':X')
   };
   
-  var payload = '{"solution_category":{"name":"API test","description":"API related documents"},"tags":{"name":""}}';
+  var payload = '{"helpdesk_ticket":{"description":"Description ...","subject":"Title","email":"example@example.com","priority":1,"status":2},"cc_emails":"example1@example.com"}';
+  var url = ENDPOINT + '/helpdesk/tickets.json';
   
-  var url = ENDPOINT + '/solution/categories.json';
   var options = {
     'method': 'post',
     'headers': headers,
     'payload': payload,
     muteHttpExceptions: true
   };
+  
   var response = UrlFetchApp.fetch(url, options);
   
   Logger.log("code: " + response.getResponseCode());
   Logger.log("text: " + response.getContentText());
-
 }
+
