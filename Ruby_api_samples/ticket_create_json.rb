b3bd6605f4ada97335f778bd8f7a76bf2dac6222
Subhash Chandran <subwiz@gmail.com>
2015-01-19 17:31:04 +0530

    removed custom field ref that may not be available in target a/c

--- Ruby_api_samples/ticket_create_json.rb
+++ Ruby_api_samples/ticket_create_json.rb
@@ -5,6 +5,6 @@ require "json"
 
 site = RestClient::Resource.new("http://domain.freshdesk.com/helpdesk/tickets.json","sample@freshdesk.com","test")
 
-response = site.post({:helpdesk_ticket=>{:description=>"Test ticket creation with attachments",:subject=>"new ticket sample",:email=>"test@abc.com",:custom_field=>{:license_1=>"ABCDEF"}},:cc_emails=>"myemail@gmail.com,test@gmail.com"},:content_type=>"application/json")
+response = site.post({:helpdesk_ticket=>{:description=>"Test ticket creation with attachments",:subject=>"new ticket sample",:email=>"test@abc.com"},:cc_emails=>"myemail@gmail.com,test@gmail.com"},:content_type=>"application/json")
 
 puts "response: #{response.code} \n #{response.body}"
