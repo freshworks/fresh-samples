927d4e0a09c72415f0b2f275ecb0684f4905561c
Subhash Chandran <subwiz@gmail.com>
2015-06-05 13:30:49 +0530

    minor formatting corrections.

--- nodejs_samples/create_ticket_multiple_attachments.js
+++ nodejs_samples/create_ticket_multiple_attachments.js
@@ -4,8 +4,9 @@ var unirest = require('unirest');
 var fs = require('fs');
 
 var PROTOCOL = "https://";
-var API_KEY = "your_api_key";
-var FD_ENDPOINT = "your_domain.freshdesk.com";
+var API_KEY = "YOUR_API_KEY";
+var FD_ENDPOINT = "YOUR_DOMAIN.freshdesk.com";
+
 var PATH = "/helpdesk/tickets.json";
 var enocoding_method = "base64";
 var auth = "Basic " + new Buffer(API_KEY + ":" + 'X').toString(enocoding_method);
@@ -13,12 +14,12 @@ var URL = PROTOCOL + FD_ENDPOINT + PATH;
 
 var fields = {
   'helpdesk_ticket[email]': 'example@example.com',
-  'helpdesk_ticket[subject]':'Test Ticket subject',
-  'helpdesk_ticket[description]':'This is a sample ticket'
+  'helpdesk_ticket[subject]': 'Ticket subject',
+  'helpdesk_ticket[description]': 'Ticket description.'
 }
 
 var headers = {
-  'Authorization':auth
+  'Authorization': auth
 }
 
 unirest.post(URL)
