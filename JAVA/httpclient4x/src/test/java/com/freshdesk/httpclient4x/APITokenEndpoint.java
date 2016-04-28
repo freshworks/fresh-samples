377cd7a113d640993a1d9d5dc5e25422037eb90c
Rashmi <rashmi@freshdesk.com>
2016-04-28 18:54:27 +0530

    Remove test files from java samples
    Signed-off-by: Rashmi <rashmi@freshdesk.com>

--- JAVA/httpclient4x/src/test/java/com/freshdesk/httpclient4x/APITokenEndpoint.java
+++
@@ -1,6 +0,0 @@
-package com.freshdesk.httpclient4x;
-
-public class APITokenEndpoint {
-    static final String apiToken = "YOURTOKEN";
-    static final String apiEndpoint = "https://YOURDOMAIN.freshdesk.com";
-}
