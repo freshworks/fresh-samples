0901fc9089794d049a7b1ec0815220c1962afca0
Rashmi <rashmi@freshdesk.com>
2016-07-21 13:13:59 +0530

    Adding sample for creating ticket with custom fields and attachments
    Signed-off-by: Rashmi <rashmi@freshdesk.com>

---
+++ NodeJs/create_ticket_custom_fields_and_attachements.js
@@ -0,0 +1,42 @@
+// Requires unirest module. To install:
+// npm install unirest
+
+var unirest = require('unirest');
+var fs = require('fs');
+
+var API_KEY = "YOUR_API_KEY";
+var FD_ENDPOINT = "YOUR_DOMAIN";
+
+var PATH = "/api/v2/tickets";
+var enocoding_method = "base64";
+var auth = "Basic " + new Buffer(API_KEY + ":" + 'X').toString(enocoding_method);
+var URL =  "https://" + FD_ENDPOINT + ".freshdesk.com"+ PATH;
+
+var fields = {
+  'email': 'email@yourdomain.com',
+  'subject': 'Ticket subject',
+  'description': 'Ticket description.',
+  'status': 2,
+  'priority': 1,
+  'custom_fields[department]':'IT'
+}
+
+var headers = {
+  'Authorization': auth
+}
+
+unirest.post(URL)
+  .headers(headers)
+  .field(fields)
+  .attach('attachments[]', fs.createReadStream('/path/to/file1.ext'))
+  .attach('attachments[]', fs.createReadStream('/path/to/file2.ext'))
+  .end(function(response){
+    console.log(response.body)
+    console.log("Response Status : " + response.status)
+    if(response.status == 201){
+      console.log("Location Header : "+ response.headers['location'])
+    }
+    else{
+      console.log("X-Request-Id :" + response.headers['x-request-id']);
+    }
+  });
